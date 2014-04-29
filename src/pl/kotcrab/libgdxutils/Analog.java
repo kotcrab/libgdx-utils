411a1ebbe547b6c445accd514c47d9c50ffd1eba
kotcrab <kotcrab@gmail.com>
2014-04-29 17:34:02 +0200

    License

--- src/pl/kotcrab/libgdxutils/Analog.java
+++ src/pl/kotcrab/libgdxutils/Analog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2012-2013 Pawel Pastuszak
+ * Copyright 2012-2014 Pawel Pastuszak
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
